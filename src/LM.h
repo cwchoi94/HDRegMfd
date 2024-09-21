2fc7b6d237658d46f0c47976976b3cfa3e06fd94
cwchoi94 <cwchoi94@gmail.com>
2024-09-21 18:14:37 +0900

    24-09-21

--- src/LM.h
+++ src/LM.h
@@ -11,11 +11,11 @@ using namespace arma;
 List LM_each(List Xorg, mat LogY, vec Ymu, String Yspace, double lambda, int Xdim_max, double R=100,
              String penalty="LASSO", double gamma=0, double phi = 1, double eta=1e-3, int max_iter=500, double threshold=1e-10);
 
-double get_loss_LM(List X, mat LogY, List Xnew_, mat LogYnew, vec Ymu, String Yspace,
-                   double lambda, int Xdim_max, double R=100, String penalty="LASSO", double gamma=0);
+double get_loss_LM(List X, mat LogY, List Xnew_, mat LogYnew, vec Ymu, String Yspace, double lambda, int Xdim_max, double R=100, 
+                   String penalty="LASSO", double gamma=0, double phi = 1);
 
 double get_loss_CV_LM(List X_, mat LogY, vec Ymu, String Yspace, double lambda, int Xdim_max, double R = 100, String cv_type = "AIC",
-                      String penalty = "LASSO", double gamma = 0);
+                      String penalty = "LASSO", double gamma = 0, double phi = 1);
 
 #endif
 
